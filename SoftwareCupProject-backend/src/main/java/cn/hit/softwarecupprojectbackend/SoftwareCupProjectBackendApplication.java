71c6518c3a5ea2024d5d6ef6d1f9b138e0d043f4
oliverchen <1031463635@qq.com>
2023-04-20 12:53:44 +0800

    First commit

---
+++ SoftwareCupProject-backend/src/main/java/cn/hit/softwarecupprojectbackend/SoftwareCupProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package cn.hit.softwarecupprojectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareCupProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareCupProjectBackendApplication.class, args);
+	}
+
+}
---
+++ SoftwareCupProject-backend/src/test/java/cn/hit/softwarecupprojectbackend/SoftwareCupProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.hit.softwarecupprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareCupProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
